fd175e378e21b501b9a85deb872aeaa16200771f
Leo Tohill <leotohill@MacBookAir.twcny.rr.com>
2024-12-04 22:35:11 -0500

    code complete

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>TestFetchResult</ion-title>
       </ion-toolbar>
     </ion-header>
 
@@ -14,21 +14,46 @@
       </ion-header>
 
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <p>
+          <ion-label>To repro, run in iOS or iOS simulator.</ion-label>
+        </p>
+        <p>
+          <ion-label>A repro will show that the fetch of test.mp3 will produce http result code 0.</ion-label>
+        </p>
+        <p style="margin-bottom: 40px;">
+          <ion-label>It should be 200.</ion-label>
+        </p>
+
+        <ion-label v-html="testResult"></ion-label>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonLabel } from '@ionic/vue';
+import { onMounted, ref } from 'vue'
+const testResult = ref('starting...');
+const doTest: any = async (): Promise<void> => {
+  let response = await fetch("test.mpthree", { method: "GET" });
+  testResult.value = `Fetch of test.mpthree http status: ${response.status}`
+  response = await fetch("test.mp3", { method: "GET" });
+  testResult.value += `<br>Fetch of test.mp3 http status: ${response.status}`
+}
+
+
+onMounted(() => {
+  doTest()
+}
+  //
+)
+
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +69,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
